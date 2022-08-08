74ce7c2617bb198cd2dc95af8e2f0e2eb922927e
Xz33M Hask <pecevgligor@protonmail.com>
2022-08-08 16:21:15 +0200

    add login dropdown menu

--- src/navigation/navbar.tsx
+++ src/navigation/navbar.tsx
@@ -1,9 +1,49 @@
+import React, { useState } from "react"
 import Button from "@mui/material/Button"
+import Menu from "@mui/material/Menu"
+import MenuItem from "@mui/material/MenuItem"
 
 export const Navbar = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
     <div className="w-full p-4 flex justify-end max-w-[1350px] mx-auto">
-      <Button variant="contained">Log in</Button>
+      <Button
+        id="demo-positioned-button"
+        aria-controls={open ? "demo-positioned-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+        variant="contained"
+      >
+        Log In
+      </Button>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "left"
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left"
+        }}
+      >
+        <MenuItem onClick={handleClose}>Student</MenuItem>
+        <MenuItem onClick={handleClose}>Talent Scout</MenuItem>
+      </Menu>
+      {/* <Button variant="contained">Log in</Button> */}
     </div>
   )
 }
