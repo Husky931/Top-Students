a50c0414b827f2024b4ba63c1301602850db11a2
Xz33M Hask <pecevgligor@protonmail.com>
2022-08-10 19:18:23 +0200

    add css baseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App"
+import CssBaseline from "@mui/material/CssBaseline"
 
 const root = createRoot(document.getElementById("root")!)
-root.render(<App />)
+root.render(
+  <CssBaseline>
+    <App />
+  </CssBaseline>
+)
--- src/navigation/navbar.tsx
+++ src/navigation/navbar.tsx
@@ -2,11 +2,7 @@ import * as React from "react"
 import Button from "@mui/material/Button"
 import Menu, { MenuProps } from "@mui/material/Menu"
 import MenuItem from "@mui/material/MenuItem"
-import EditIcon from "@mui/icons-material/Edit"
 import Divider from "@mui/material/Divider"
-import ArchiveIcon from "@mui/icons-material/Archive"
-import FileCopyIcon from "@mui/icons-material/FileCopy"
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz"
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown"
 import SchoolIcon from "@mui/icons-material/School"
 import SearchIcon from "@mui/icons-material/Search"
@@ -59,15 +55,6 @@ export default function CustomizedMenus(props: any) {
           <SearchIcon />
           Talent Scout
         </MenuItem>
-        {/* <Divider sx={{ my: 0.5 }} />
-        <MenuItem onClick={handleClose} disableRipple>
-          <ArchiveIcon />
-          Archive
-        </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
-          <MoreHorizIcon />
-          More
-        </MenuItem> */}
       </Menu>
     </div>
   )
